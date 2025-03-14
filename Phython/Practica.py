2a8301a943e7b5b940b488f9bedf20bf62aeab6d
RomelCarlof <rib2419882488@gmail.com>
2025-03-14 12:34:40 -0500

    Modificaciones del 1Mar2023

--- Phython/Practica.py
+++ Phython/Practica.py
@@ -1,10 +1,4 @@
-from decimal import Decimal, getcontext
 
-# Configurar la precisión a 1000 dígitos
-getcontext().prec = 1000
 
-# Operación con Decimal
-resultado = Decimal("0.1") + Decimal("0.1") + Decimal("0.1") - Decimal("0.3")
+bin (v)
 
-# Mostrar el resultado
-print(resultado)  # Debería imprimir 0.0
