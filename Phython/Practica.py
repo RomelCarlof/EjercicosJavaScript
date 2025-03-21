b57e503c0fa14771525fdc5b6e1aead64c9ea793
RomelCarlof <rib2419882488@gmail.com>
2025-03-21 11:19:32 -0500

    Actualización main

--- Phython/Practica.py
+++ Phython/Practica.py
@@ -10,4 +10,8 @@ resultado = Decimal("0.1") + Decimal("0.1") + Decimal("0.1") - Decimal("0.3")
 print(resultado)  # Debería imprimir 0.0
 
 V= True
-f = False
+f = False  
+
+# str.capitalize()  Regresa la lista de caracteres inicial pero poniendo la primera letra en mayúscula.
+# str.isalpha()  Regreso Verdadero si la lista de caracteres es no vacía y todos los caracteres son letras. En la figura 15 podemos ver que no regresa Falso, pues tenemos '!' que no es considerado una letra.
+# str.isdigit() Regreso Verdadero si la lista de caracteres es no vacía y todos los caracteres son números. En la figura 15 podemos ver que una cadena no está compuesta por puros números.
