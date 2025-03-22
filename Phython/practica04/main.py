be58a3f0a53340791aafb6371a1a03b82fdd7f83
RomelCarlof <rib2419882488@gmail.com>
2025-03-22 09:29:08 -0500

    Actualización main

---
+++ Phython/practica04/main.py
@@ -0,0 +1,4 @@
+# Definimos una lista de elementos
+elementos = ["Agua", "Aire", "Tierra", "Fuego"]
+
+        
---
+++ Phython/practica04/modulo.py
@@ -0,0 +1,7 @@
+
+# Importamos la variable 'elementos' desde el módulo
+from modulo import elementos 
+
+
+# Creamos una lista con los elementos
+print("Lista de elementos: ", elementos)
