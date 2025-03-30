09b61005d76be114b1e303b904482b5606260188
RomelCarlof <rib2419882488@gmail.com>
2025-03-29 19:45:23 -0500

    Actualizacion main

--- Phython/practica03.py
+++ Phython/practica03.py
@@ -1,13 +1,19 @@
 class Persona:
-    def __init__(self, nombre, apellido):  # Constructor que recibe un nombre
+    def __init__(self, nombre, apellido, direccion, telefono):  # Constructor que recibe un nombre
         self.__nombre = nombre; # Almacena el nombre en el objeto
-        self.__apellido = apellido;
+        self.__apellido = apellido; 
+        self.__direccion = direccion; 
+        self.__telefono = telefono; 
       #  print(self.__apellido);
     
     def mostrar__nombre(self):  # Método correcto para mostrar el nombre
-        print('Tu nombre es: ' + self.__nombre + ' ' + self.__apellido);
+        print('Tu nombre es: ' + self.__nombre + ' ' + self.__apellido); 
+        # print(f'Tu nombre es: {self.__nombre} {self.__apellido}')  # Usar f-strings para mejor legibilidad
+
+    def mostrar__direccion(self):  # Método correcto para mostrar la dirección
+        print('Tu direccion es: ' + self.__direccion + ' ' + self.__telefono); 
         # print(f'Tu nombre es: {self.__nombre} {self.__apellido}')  # Usar f-strings para mejor legibilidad
 
 # Crear un objeto de la clase Persona
-profesor = Persona('Romel' , 'IZAGUIRRE BARBARAN');
+profesor = Persona('Romel' , 'IZAGUIRRE BARBARAN', 'MICAELA BASTIDA N° 103', '994847647'); 
 profesor.mostrar__nombre();  # Llamar correctamente al método
