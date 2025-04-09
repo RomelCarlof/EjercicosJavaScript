0586ec544e3f925223d5b4cea94aa26b8195f063
RomelCarlof <rib2419882488@gmail.com>
2025-04-09 15:57:12 -0500

    Actualizacion de la version

---
+++ Phython/practica/ejercicio03.py
@@ -0,0 +1,20 @@
+# Declarar variables
+"""let nummero;
+let ultimoDigito;"""
+
+# Solicitar entrada al usuario
+numero =int(input("Ingrese un numero:"))
+
+
+# Evaluar 
+
+ultimoDigito = numero % 10
+
+if  ultimoDigito == 4:
+    
+   print("El numero termina en 4")
+
+else:
+
+    print(" El numero no termina en 4")
+
