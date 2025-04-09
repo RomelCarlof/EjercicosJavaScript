14d65cd3a0d055fe5651d512cada14cc643246d4
RomelCarlof <rib2419882488@gmail.com>
2025-04-09 16:02:16 -0500

    Actualizacion de la version

---
+++ Phython/practica/ejercicio04.py
@@ -0,0 +1,34 @@
+//4. Hacer un algoritmo en Pseint que lea tres números enteros y los muestre de menor a mayor.
+"""let num01;
+let num02;
+let num03;
+let Intercambio;"""
+
+
+# Solicitar entrada al usuario
+
+num01=int(input("Ingrese el primer número:"))
+
+num02=int(input("Ingrese el segundo número:"))
+
+
+num03=int(input("Ingrese el segundo número:"))
+
+if num01 > num02:
+    Intercambio = num01
+    num01 = num02
+    num02 = Intercambio
+
+
+if num02 > num03:
+    Intercambio = num02
+    num02 = num03
+    num03 = Intercambio
+
+
+if num01 > num02:
+    Intercambio = num01
+    num01 = num02
+    num02 = Intercambio;
+
+print("Los numeros ordenados son: " + num01 + "," +  num02 + "," + num03 );
