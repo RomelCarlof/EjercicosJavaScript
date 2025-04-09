2c08d85656035c20be821f1534664aadbdc1cf17
RomelCarlof <rib2419882488@gmail.com>
2025-04-09 16:16:53 -0500

    Actualizacion de la version

--- Phython/practica/ejercicio04.py
+++ Phython/practica/ejercicio04.py
@@ -31,4 +31,4 @@ if num01 > num02:
     num01 = num02
     num02 = Intercambio
 
-print("Los números ordenados son:", num01, num02, num03)
+print("Los números ordenados son:", num01, ",", num02, ",", num03)
