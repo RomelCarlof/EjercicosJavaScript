edd07ad6700930d83121f8d6be60e8da36ef215d
RomelCarlof <rib2419882488@gmail.com>
2025-04-09 15:10:04 -0500

    Continuando con el avance

---
+++ Phython/practica/ejercicio01.py
@@ -0,0 +1,24 @@
+# Solicita los números al usuario
+numero1 = float(input("Escribe el primer número a sumar: "))
+numero2 = float(input("Escribe el segundo número a sumar: "))
+
+# Suma los números
+resultado = numero1 + numero2
+
+# Muestra el resultado
+print("El resultado es", resultado)
+
+
+"""Proceso SUNA
+	Numero1 = 0
+	Numero2 = 0
+	Escribir "Escribe el primer numero a sumar"
+	
+	Leer Numero1
+	
+	Escribir "Escribe el segundo numero a sumar"
+	
+	Leer Numero2
+	
+	resultado = Numero1+ Numero2
+	escribir "El resultado es " resultado"""
