0c37e454eed6f51e78962121d93be031474712be
RomelCarlof <rib2419882488@gmail.com>
2025-04-10 17:07:16 -0500

    Continuando con la actualizacion

---
+++ Phython/practica/ejercicio11.py
@@ -0,0 +1,26 @@
+
+
+num01 = float(input("Ingrese el primer número:"))
+num02 = float(input("Ingrese el segundo número:"))
+num03 = float(input("Ingrese el tercer número:"))
+
+# Ordenamiento en orden descendente (mayor a menor)
+if num01 < num02: 
+    temp = num01
+    num01 = num02
+    num02 = temp
+
+
+if num02 < num03:
+    temp = num02
+    num02 = num03
+    num03 = temp
+
+if num01 < num02:
+    temp = num01
+    num01 = num02
+    num02 = temp
+
+
+# Mostrar el resultado correctamente separado
+print("Los numeros ordenados son:" , num01 , "," , num02 , ","  , num03)
