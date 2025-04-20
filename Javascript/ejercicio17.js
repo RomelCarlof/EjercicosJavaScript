0080255d81c1c72f51ea67d8eae17fab34adccb3
RomelCarlof <rib2419882488@gmail.com>
2025-04-20 17:54:30 -0500

    Actualizacion de Version

---
+++ Javascript/ejercicio17.js
@@ -0,0 +1,36 @@
+// Algoritmo CalcularHora
+let horas;
+let minutos;
+let segundoss;
+
+
+//Pedir al usuario que ingrese la hora actual
+horas = parseInt(prompt("Ingrrese la hora (0-23):"));
+
+minutos = parseInt(prompt("Ingrrese los minutos (0-59):"));
+
+segundoss = parseInt(prompt("Ingrrese los segundos (0-59):"));
+
+
+//Sumar en segundos 
+segundoss = segundoss + 1;
+
+//Si los segundos llegan a 60, reiniciar y sumar un minuto
+if (segundoss = 60) {
+    segundoss = 0;
+    minutos = minutos + 1;
+}
+//Si los  minutos llegan a 60, reiniciar y sumar una hora
+if (minutos = 60) {
+    minutos = 0;
+    horas = horas + 1;
+}
+//Si las horas llegan a 24, reiniciar el día
+if (horas = 24) {
+    horas = 0;
+}
+
+//Mostrar la hora actualizada
+	
+	console.log("La hora dentro de un segundo es: "+ horas+ ":" +minutos+ ":" +segundoss);
+    alert("La hora dentro de un segundo es: "+ horas+ ":" +minutos+ ":" +segundoss);
