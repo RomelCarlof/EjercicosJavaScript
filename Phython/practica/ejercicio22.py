76c9be45c6d3fb36e80e33f505d00eff94a7dc06
RomelCarlof <rib2419882488@gmail.com>
2025-04-28 20:15:26 -0500

    Actualización main

---
+++ Phython/practica/ejercicio22.py
@@ -0,0 +1,22 @@
+#Algoritmo ContadorPara
+
+entrada = input("Ingrese un número entero: ")
+
+try:
+
+    n = int(entrada)
+    print("¡No ingresaste un número válido!")
+
+    if n < 0:
+        print("La suma no está definida para números negativos.")
+
+    else:
+        suma = 0 # Inicia el número entero
+        for i in range(1, n+1):
+         suma = suma + i  #Sumar  
+    #FinPara
+        print(f"El factorial de {n} es {suma}")
+
+except ValueError:
+    print("¡No ingresaste un número válido!")
+   
