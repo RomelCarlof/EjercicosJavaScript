c2e52059b2cfc7deed9836df4d7837f41bda2951
RomelCarlof <rib2419882488@gmail.com>
2025-05-06 19:38:58 -0500

    Actualización de main

---
+++ Phython/practica/ejercicio23.py
@@ -0,0 +1,22 @@
+
+
+entrada = input("Ingrese un número entero positivo")
+
+try:
+
+    n = int(entrada)
+    print("¡No ingresaste un número válido!")
+
+    if n < 0:
+        print("¡No ingresaste un número positivo!")
+
+    else:
+        Suma = 0
+        for i in range(1, n + 1):
+            Suma += i
+    #FinPara
+    print("La suma de los números enteros impares menores o iguales a " + n + " es:  " + Suma);
+    
+except ValueError:
+    print("¡No ingresaste un número válido!")
+#inAlgoritmo
