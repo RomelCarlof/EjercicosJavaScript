0372ff4bcf5d935358712ad66f364cf577b80f19
RomelCarlof <rib2419882488@gmail.com>
2025-05-06 19:47:31 -0500

    Actualización de main

--- Phython/practica/ejercicio23.py
+++ Phython/practica/ejercicio23.py
@@ -7,12 +7,12 @@ try:
     n = int(entrada)
     print("¡No ingresaste un número válido!")
 
-    if n < 0:
+    if n <= 0:
         print("¡No ingresaste un número positivo!")
 
     else:
         Suma = 0
-        for i in range(1, n + 1):
+        for i in range(1, n + 1, 2):
             Suma += i
     #FinPara
     print("La suma de los números enteros impares menores o iguales a " , n , " es:  " , Suma)
