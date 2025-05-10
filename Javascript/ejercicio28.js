1964c5fe7666f020d3d141324628bf11763a90bb
RomelCarlof <rib2419882488@gmail.com>
2025-05-10 18:35:40 -0500

    actualizacion de version

---
+++ Javascript/ejercicio28.js
@@ -0,0 +1,17 @@
+//Proceso Suma_cIEN_Numeros
+let suma;
+let i;
+
+i = 1
+suma = 0
+
+do {
+        console.log("i vale: " + i);
+        //alert("i vale: " + i);
+        suma = suma + i
+        i = i + 1;// Incrementa i
+           
+} while (i <= 100);
+console.log("La suma de los 100 primeros numeros es: " + suma);
+alert("La suma de los 100 primeros numeros es: " + suma);
+//FinProceso
