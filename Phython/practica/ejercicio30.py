08abdb0f40f02efe30bfe583ed540f804bdafee7
RomelCarlof <rib2419882488@gmail.com>
2025-05-10 19:43:16 -0500

    Actualizacipon main

---
+++ Phython/practica/ejercicio30.py
@@ -0,0 +1,16 @@
+#Proceso Suma_Cien_Primeros_Numeros_Ciclo_Para
+
+#suma;
+# i;
+suma = 0
+
+for i in range (0, 301, 2):
+
+	print("Escribir: " +i)
+	suma += i
+	i += 1
+
+	#Fin Para
+	print("La suma de los 100 primeros numeros es: " + suma)
+    
+#FinProceso
