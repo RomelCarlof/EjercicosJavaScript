b45a4384f5524536944857d4560f76bb3aef26af
RomelCarlof <rib2419882488@gmail.com>
2025-05-10 19:47:48 -0500

    Actualizacipon main

--- Phython/practica/ejercicio30.py
+++ Phython/practica/ejercicio30.py
@@ -6,11 +6,11 @@ suma = 0
 
 for i in range (0, 301, 2):
 
-	print("Escribir: " +i)
+	print(f"Escribir: {i}")
 	suma += i
-	i += 1
+	
 
-	#Fin Para
-	print("La suma de los 100 primeros numeros es: " + suma)
+#Fin Para
+print("La suma de los 100 primeros numeros es:" , suma)
     
 #FinProceso
