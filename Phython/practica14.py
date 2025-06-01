ab83ebba0517441169ef703b539d123d597b5511
RomelCarlof <rib2419882488@gmail.com>
2025-06-01 17:54:49 -0500

    actualización del merge

---
+++ Phython/practica14.py
@@ -0,0 +1,14 @@
+def cambio_voraz(cantidad, monedas):
+    resultado = []
+    for moneda in monedas:
+        while cantidad >= moneda:
+            cantidad -= moneda
+            resultado.append(moneda)
+    return resultado
+
+# Prueba
+monedas = [50, 20, 10, 5, 1]
+cambio = cambio_voraz(87, monedas)
+
+print("Monedas usadas:", cambio)
+print("Total de monedas:", len(cambio))
