2218c97d31d3d6b9741b88d740d705ebb85d62c7
RomelCarlof <rib2419882488@gmail.com>
2025-06-17 10:11:19 -0500

    Actualizacion

--- Phython/ejercicios_funciones/ejercicio10.py
+++ Phython/ejercicios_funciones/ejercicio10.py
@@ -2,7 +2,7 @@
 #9. Escribe una función que compruebe si un string dado es un palíndromo. Un palíndromo es una secuencia de caracteres que se lee igual de izquierda a derecha que de derecha a 
 #izquierda. Por ejemplo, la función devolverá True si recibe el string "reconocer" y False si recibe el string "python".
 
-def es_palindromo(s)
+def es_palindromo(s):
     for i in range(len(s)//2):
         if s[i] != s[len(s)- i - 1]:
             return False
