e7f5fd12bed653d28d82b06ea5e97bc7f263896d
RomelCarlof <rib2419882488@gmail.com>
2025-06-18 17:13:23 -0500

    Actualizacion main

--- Phython/Complejidad_algoritmica.py/Divide_Venceras}/ejercicio01.py
+++ Phython/Complejidad_algoritmica.py/Divide_Venceras}/ejercicio01.py
@@ -12,6 +12,7 @@ def contar_pares(arr, k):
     for i in izquierda:
         for j in derecha:
             if i + j == k:
+                print(f"Par encontrado: ({i}, {j})")
                 conteo_cruzado += 1
 
     # Llamadas recursivas
@@ -19,3 +20,9 @@ def contar_pares(arr, k):
     conteo_derecha = contar_pares(derecha, k)
 
     return conteo_cruzado + conteo_izquierda + conteo_derecha
+
+
+arr = [1, 5, 3, 7, 2, 4]
+k = 6
+total = contar_pares(arr, k)
+print(f"Total de pares que suman {k}: {total}")
