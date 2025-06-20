e5d9b0b342610f09c43e9f81593aa0b8aa92bb9c
RomelCarlof <rib2419882488@gmail.com>
2025-06-20 18:05:00 -0500

    Actualizacion del main

--- Phython/File/ejercicio.py
+++ Phython/File/ejercicio.py
@@ -9,14 +9,14 @@ import os  # 👈 IMPORTANTE
    # return lista
 
 # Crear ruta relativa del archivo
-ruta = os.path.join("res", "Link_Paginas.txt")
+ruta = os.path.join("res", "Ejercicio3.txt")
 
 # Crear carpeta 'res' si no existe
 os.makedirs(os.path.dirname(ruta), exist_ok=True)
 
 # Escribir en el archivo
 with open(ruta, 'wt', encoding='utf-8') as fichero:
-    fichero.write("https://www.ejemplo.com\n")
-    fichero.write("https://otra-url.com\n")
+    fichero.write("")
+    fichero.write("")
 
 print("✅ Archivo creado correctamente en:", ruta)
---
+++ Phython/File/ejercicio03.py
@@ -0,0 +1,19 @@
+# 3. Escribe una función que reciba una ruta de un fichero de texto devuelva un diccionario con la frecuencia de aparición de cada palabra. 
+# Ejemplo: un fichero que contenga la frase 'es mejor que venga que que no venga' devolverá el siguiente diccionario: {'es' : 1, 'mejor' : 1, 'que' : 3, 'venga' : 2, 'no' : 1}.
+# Para dividir un string en palabras puedes hacer uso del método split.
+
+def anyadir_palabra(frecuencias, palabra):
+    if palabra in frecuencias:
+        frecuencias[palabra] += 1
+    else:
+        frecuencias[palabra] = 1
+
+def obtener_frecuencias(ruta_fichero):
+    frecuencias = {}
+    with open(ruta_fichero) as fichero:
+        for linea in fichero:
+            for palabra in linea.split():
+                anyadir_palabra(frecuencias, palabra)
+    return frecuencias
+
+obtener_frecuencias('res/Ejercicio3.txt')
